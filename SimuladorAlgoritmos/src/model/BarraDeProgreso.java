ba69d1c537dca3aa8c721cd9850ec563f7bb3b35
Chango <a16001581@alumnos.uady.mx>
2022-05-11 20:41:52 -0500

    Primer commit

---
+++ SimuladorAlgoritmos/src/model/BarraDeProgreso.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+
+*/
+package model;
+import java.awt.*;
+import javax.swing.*;
+
+
+/**
+ *
+ * @author PC GOOSE
+ */
+public class BarraDeProgreso {
+    JFrame frame = new JFrame();
+    JProgressBar bar = new JProgressBar();
+    
+    public BarraDeProgreso(){
+        bar.setValue(0);
+        bar.setBounds(0,0,420,50);
+        bar.setStringPainted(true);
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 420);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+    
+}
